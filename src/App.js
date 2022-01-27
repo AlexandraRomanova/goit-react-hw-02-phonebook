e1275148ec806d9ad6430b380aaa230bb11f4dfa
AlexandraRomanova <65874420+AlexandraRomanova@users.noreply.github.com>
2022-01-27 15:25:20 +0200

    hw

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ import PropTypes from "prop-types";
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: ''
+    filter: "",
   };
 
-  static propTypes = {}
+  static propTypes = {};
 
   addContact = ({ name, number }) => {
     const newContact = {
       id: nanoid(),
       name,
       number,
-    }
+    };
 
     // const duplicateContactName = this.state.contacts.find(contact => contact.name === newContact.name);
     // const duplicateContactNumber = this.state.contacts.find(contact => contact.number === newContact.number);
@@ -38,25 +38,27 @@ class App extends Component {
     //    return;
     // };
 
+    this.setState(({ contacts }) => ({ contacts: [newContact, ...contacts] }));
+  };
 
-    this.setState(({contacts}) => ({ contacts: [newContact, ...contacts]}))
-  }
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
+    }));
+  };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId)
-    }))
-  }
-
-  changeFilter = e => {
+  changeFilter = (e) => {
     this.setState({ filter: e.currentTarget.value });
   };
 
   getVisibleContacts = () => {
     const normalizedFilter = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
-  }
-
+    return this.state.contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
 
   render() {
     const visibleContacts = this.getVisibleContacts();
@@ -65,11 +67,14 @@ class App extends Component {
         <h1>Phonebook</h1>
         <PhoneEditor onSubmit={this.addContact} contacts={visibleContacts} />
         <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.changeFilter}/>
-        <PhoneList contacts={visibleContacts} onDeleteContact={this.deleteContact} />
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <PhoneList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
       </div>
     );
   }
-};
+}
 
 export default App;
